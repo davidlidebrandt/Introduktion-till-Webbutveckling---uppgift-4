3560d8bc4cc158e7c6cd5be6ae48f356cc68ad7c
David Lidebrandt <david.lidebrandt@gmail.com>
2022-09-26 17:45:34 +0200

    Add styling

--- main.js
+++ main.js
@@ -1,40 +1,44 @@
-// Min egen lösning som dock inte lagras/hanteras i en array med objekt
+const addToDoButton = document.querySelector("#addToDoButton");
+const addToDoInput = document.querySelector("#toDoInput");
+const toDoList = document.querySelector("#toDoList");
+const numberOfFinishedToDos = document.querySelector("#numberOfFinishedToDos");
+const list = []
+const test = document.createElement("li")
+list.push({test:test})
+console.log(list[0]["test"])
 
-// const addToDoButton = document.querySelector("#addToDoButton");
-// const addToDoInput = document.querySelector("#toDoInput");
-// const toDoList = document.querySelector("#toDoList");
-// const numberOfFinishedToDos = document.querySelector("#numberOfFinishedToDos");
+addToDoButton.addEventListener("click", toDoListener)
 
-// addToDoButton.addEventListener("click", toDoListener)
-
-// function toDoListener() {
-//     if (addToDoInput.value === "") {
-//         alert("Write something");
-//       } else {
-//         let currentlyFinishedItems = parseInt(numberOfFinishedToDos.textContent);
-//         const trashCan = document.createElement("img");
-//         trashCan.src = "trashcan.svg";
-//         const toDoItem = document.createElement("li");
-//         toDoItem.textContent = addToDoInput.value;
-//         toDoItem.append(trashCan);
-//         toDoList.append(toDoItem);
-//         addToDoInput.value = "";
-//         trashCan.addEventListener("click", (event)=> {
-//             trashCan.remove();
-//             toDoItem.remove();
-//             currentlyFinishedItems--;
-//             numberOfFinishedToDos.textContent = currentlyFinishedItems >= 0 ? currentlyFinishedItems : 0;
-//             event.stopPropagation();
-//         })
-//         toDoItem.addEventListener("click", () => {
-//           if (toDoItem.classList.contains("finishedToDo")) {
-//             toDoItem.classList.remove("finishedToDo");
-//             currentlyFinishedItems--;
-//           } else {
-//             toDoItem.classList.add("finishedToDo");
-//             currentlyFinishedItems++;
-//           }
-//           numberOfFinishedToDos.textContent = currentlyFinishedItems;
-//         });
-//       }
-//     }
+function toDoListener() {
+    if (addToDoInput.value === "") {
+        alert("Write something");
+      } else {
+        let currentlyFinishedItems = parseInt(numberOfFinishedToDos.textContent);
+        const trashCan = document.createElement("img");
+        trashCan.src = "trashcan.svg";
+        const toDoItem = document.createElement("li");
+        toDoItem.textContent = addToDoInput.value;
+        toDoItem.append(trashCan);
+        toDoList.append(toDoItem);
+        addToDoInput.value = "";
+        trashCan.addEventListener("click", (event)=> {
+            trashCan.remove();
+            toDoItem.remove();
+            if (toDoItem.classList.contains("finishedToDo")) {
+                currentlyFinishedItems--;
+            }
+            numberOfFinishedToDos.textContent = currentlyFinishedItems >= 0 ? currentlyFinishedItems : 0;
+            event.stopPropagation();
+        })
+        toDoItem.addEventListener("click", () => {
+          if (toDoItem.classList.contains("finishedToDo")) {
+            toDoItem.classList.remove("finishedToDo");
+            currentlyFinishedItems--;
+          } else {
+            toDoItem.classList.add("finishedToDo");
+            currentlyFinishedItems++;
+          }
+          numberOfFinishedToDos.textContent = currentlyFinishedItems;
+        });
+      }
+    }
