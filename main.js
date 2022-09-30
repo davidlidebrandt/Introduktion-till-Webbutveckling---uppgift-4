060ac7a78ac248392505f06dc8988d9a75cfeba5
David Lidebrandt <david.lidebrandt@gmail.com>
2022-09-30 21:56:23 +0200

    Add todo array

--- main.js
+++ main.js
@@ -2,41 +2,68 @@ const addToDoButton = document.querySelector("#addToDoButton");
 const addToDoInput = document.querySelector("#toDoInput");
 const toDoList = document.querySelector("#toDoList");
 const numberOfFinishedToDos = document.querySelector("#numberOfFinishedToDos");
+let toDoItems = [];
 let currentlyFinishedItems = parseInt(numberOfFinishedToDos.textContent);
-addToDoButton.addEventListener("click", toDoListener)
+
+addToDoButton.addEventListener("click", toDoListener);
 
 function toDoListener() {
-    if (addToDoInput.value === "") {
-        alert("Write something");
-      } else {
-        console.log(currentlyFinishedItems)
-        const trashCan = document.createElement("img");
-        trashCan.src = "trashcan.svg";
-        const toDoItem = document.createElement("li");
-        toDoItem.textContent = addToDoInput.value;
-        toDoItem.append(trashCan);
-        toDoList.append(toDoItem);
-        addToDoInput.value = "";
-        trashCan.addEventListener("click", (event)=> {
-            trashCan.remove();
-            toDoItem.remove();
-            if (toDoItem.classList.contains("finishedToDo")) {
-                currentlyFinishedItems--;
-            }
-            numberOfFinishedToDos.textContent = currentlyFinishedItems >= 0 ? currentlyFinishedItems : 0;
-            event.stopPropagation();
-        })
-        toDoItem.addEventListener("click", () => {
-          if (toDoItem.classList.contains("finishedToDo")) {
-            toDoItem.classList.remove("finishedToDo");
-            currentlyFinishedItems--;
-          } else {
-            toDoItem.classList.add("finishedToDo");
-            currentlyFinishedItems++;
-            console.log(currentlyFinishedItems)
-          }
-          numberOfFinishedToDos.textContent = currentlyFinishedItems;
-          console.log(currentlyFinishedItems)
-        });
+  if (addToDoInput.value === "") {
+    alert("Write something");
+  } else {
+    const toDoId = Date.now();
+    toDoItems.push({ text: addToDoInput.value, completed: false, id: toDoId });
+    const trashCan = document.createElement("img");
+    trashCan.src = "trashcan.svg";
+    const toDoItem = document.createElement("li");
+    toDoItem.textContent = addToDoInput.value;
+    const hiddenToDoId = document.createElement("span");
+    hiddenToDoId.setAttribute("hidden", true);
+    hiddenToDoId.textContent = toDoId;
+    toDoItem.append(trashCan);
+    toDoItem.append(hiddenToDoId);
+    toDoList.append(toDoItem);
+    addToDoInput.value = "";
+    console.log(toDoItems)
+
+    trashCan.addEventListener("click", (event) => {
+      let toDoId = parseInt(trashCan.nextSibling.textContent);
+      toDoItems = toDoItems.filter((toDo) => {
+        return toDo["id"] !== toDoId
+      });
+      trashCan.remove();
+      toDoItem.remove();
+      if (toDoItem.classList.contains("finishedToDo")) {
+        currentlyFinishedItems--;
       }
-    }
+      numberOfFinishedToDos.textContent =
+        currentlyFinishedItems >= 0 ? currentlyFinishedItems : 0;
+      event.stopPropagation();
+      console.log(toDoItems)
+    });
+
+    toDoItem.addEventListener("click", () => {
+      let toDoId = parseInt(trashCan.nextSibling.textContent);
+      if (toDoItem.classList.contains("finishedToDo")) {
+        toDoItems.forEach((toDo, index) => {
+          if(toDo["id"] === toDoId) {
+            toDoItems[index]["completed"] = false;
+          }
+        });
+        toDoItem.classList.remove("finishedToDo");
+        currentlyFinishedItems--;
+      } else {
+        toDoItems.forEach((toDo, index) => {
+          if(toDo["id"] === toDoId) {
+            toDoItems[index]["completed"] = true;
+          }
+        });
+        toDoItem.classList.add("finishedToDo");
+        currentlyFinishedItems++;
+        console.log(currentlyFinishedItems);
+      }
+      numberOfFinishedToDos.textContent = currentlyFinishedItems;
+      console.log(toDoItems)
+    });
+  }
+}
